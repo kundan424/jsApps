b32e1469dfbbbe4ddf8ff15d3fcda01e2fab8ba4
kundan kumar <kundankumar64355@gmail.com>
2024-10-27 11:57:46 +0530

    second commit

--- test.js
+++ test.js
@@ -2,6 +2,7 @@ console.log(" hello")
 var a = 9
 console.log(a )
 console.log( "a ")
+console.log(" FIRST COMMIT")
 
 var a = 10 
  a >= 0 ?  (a > 5 ? console.log(" number is greater then 0 nad 5") : console.log( " the nuber is greater than 0 but  less than 5")) : console.log( "nuumber is less than 0")
