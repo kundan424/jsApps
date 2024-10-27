e831bf0827eef15399644427b1405fe7042c31ac
kundan kumar <kundankumar64355@gmail.com>
2024-10-27 11:01:41 +0530

    first commit

---
+++ test.js
@@ -0,0 +1,7 @@
+console.log(" hello")
+var a = 9
+console.log(a )
+console.log( "a ")
+
+var a = 10 
+ a >= 0 ?  (a > 5 ? console.log(" number is greater then 0 nad 5") : console.log( " the nuber is greater than 0 but  less than 5")) : console.log( "nuumber is less than 0")
